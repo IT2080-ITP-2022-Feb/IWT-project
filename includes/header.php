a7244bce8298498e2daa9c06cc3a30a4f5487f36
NTharuka <83983046+NTharuka@users.noreply.github.com>
2021-05-16 15:17:25 +0530

    a

--- includes/header.php
+++ includes/header.php
@@ -8,8 +8,7 @@
 	<body>
 	<img src="images/logo1.png" align="left" width="100px" hight="50px" alt="DSK book store logo">
 		<h1>DSK Book Store</h1>
-		<li style="position:absolute !important; right:12px !important;" class="menu">
-		<div class="topnav">
+		
 		<input type="button" value="Sign Up" width="100px" hight="20px"><br/>
 		<input type="button" value="Sign In " width="100px" hight="20px"><br/>
 		</div></li>
--- pages/about.php
+++ pages/about.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="../css/about.css">
 
 <h1>About Page</h1>
-<h1> Hello hi</h1>
+<h1> Hello project</h1>
 
 
 <script src="../js/about.js"></script>
