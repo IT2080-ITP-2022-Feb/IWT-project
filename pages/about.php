bec125e504b7309bff1404eb9b021c709ae8fbfe
SewwandiDinu <dinushikadodanthenna@gmail.com>
2021-05-18 04:49:52 +0530

    Update about.php
    create body part

--- pages/about.php
+++ pages/about.php
@@ -4,8 +4,39 @@
 
 <link rel="stylesheet" href="../css/about.css">
 
-<h1>About Page</h1>
-<h1> Hello project</h1>
+<head>
+<style>
+fieldset{
+     background-color:#87CEFA;height:550px;width:600px;
+     float:center;font-size:18pt;
+     }
+input{
+     margin:10px;
+     }
+
+</style>
+
+    <title>About us page</title>
+</head>
+<body>
+<h1 align="center">About us</h1>
+
+<fieldset>
+
+<p>DSK is an online book store for used books,that operates from 
+   home avid reader who lacks time to spend inside a local book
+   shop due to the busy work schedule,because not all the time you will
+    manage to find the best deal or extract title for what you're
+   looking for in a minute? Don't worry. Here we are. DSK is the
+    leading online book store in Sri Lanka which offers you an oustanding 
+   professional and reliable service with a great range of books. We have
+   the best must read collection of books written by the versatile writers
+  in Sri Lanka and the writers all over the world.</p> 
+
+</fieldset> 
+
+</body>
+
 
 
 <script src="../js/about.js"></script>
