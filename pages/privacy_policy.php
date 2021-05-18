eacef2bc0ae7945a7c322f825dab43fb5bcb5b64
SewwandiDinu <dinushikadodanthenna@gmail.com>
2021-05-18 05:38:00 +0530

    Create privacy_policy.php
    create privacy policy page by using html

---
+++ pages/privacy_policy.php
@@ -0,0 +1,52 @@
+<?php
+	include "../includes/header.php";
+?>
+
+<link rel="stylesheet" href="../css/about.css">
+
+
+
+
+
+<head>
+<title> Privacy Policy</title>
+
+</head>
+<body>
+
+<h1 align="center">Privacy Policy<h1>
+<h4>Our Privacy Policy explain what personal information we collect about
+    you use the Website.</h4> 
+<p>We are collect,</p>
+
+<h4>We may collect the following information.</h4>
+
+<ul type="bullet">
+   <li>Name</li>
+   <li>Book title</li>
+   <li>Contact information including email address</li>
+   <li>Your user name and password</li>
+   <li>Your communication</li>
+</ul>
+
+
+<h2>Security</h2>
+<p>We are committed to ensuring that your information is secure.In order to
+   prevent unauthorised accsess or disclosure,we have put in place suitable 
+   physical,electronic and managerial procedures to safeguard and secure the 
+   information we collect online </p>
+
+<h2>Changes to this statement</h2>
+<p>DSK will occasionally update this Privacy Policy to reflect company and
+   customer feedback.DSK encourages you to periodically review this statement
+   to be informed of how DSK is protecting your information. This policy was 
+   last update in March 2021</p>
+
+
+</body>
+
+<script src="../js/about.js"></script>
+<?php
+	include "../includes/footer.php";
+?>
+
