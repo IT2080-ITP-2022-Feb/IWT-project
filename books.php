9a1720e9a32691cec2c33e9da44f94a2245962a3
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-05-24 09:37:40 +0530

    Write Some PHP

--- books.php
+++ books.php
@@ -5,12 +5,27 @@
 
 <link rel="stylesheet" href="./css/searchResultPage.css">
 
-<title>Books</title>
-
 
 <!-- Type your Code here -->
 <h1>Books Page</h1>
+<?php
+    $sql = "SELECT *
+    FROM Book";
 
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        echo "
+          <h4>".$row['Book_name']."</h4>
+          <a href='./cart.php?bookID=".$row['Book_ID']."'>Add to Cart</a>
+          ";
+      }
+    }
+    else {
+      echo "0 items";
+    }
+?>
 
 
 <script src="./js/searchResultPage.js"></script>
---
+++ src/dbConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = 'root';
+$password = "";
+$dbName = "book_store";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbName);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
