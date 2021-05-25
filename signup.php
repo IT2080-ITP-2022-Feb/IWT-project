224b83be36477ed5825350ee063156510313782f
SRashani <83983242+SRashani@users.noreply.github.com>
2021-05-25 22:10:33 +0530

    Samee

--- signup.php
+++ signup.php
@@ -7,9 +7,40 @@
 
 
 <!-- Type your Code here -->
-<h1>Sign Up Page</h1>
+
+<form action="signup.php" method="post">
+		<div class="container">
+	    <h1><center>Sign Up </center></h1>
+		<p><center>Lets get you on board</center></p>
+		<br/>
+        <b>User type:</b>
+        <input name="Usertype" type="radio" value="Buyer">Buyer
+		<input name="Usertype" type="radio" value="Seller" checked="checked">Seller <br/><br/>
+		<lable for="FirstName"><b>First Name</b></lable><br/>
+        <input text="FirstName" type="text">
+		<lable for="LastName"><b>Last Name</b></lable>
+        <input text="LastName" type="text" >
+        <br/><br/>		
+        <lable for="ContactNumber"><b>Contact Number</b></lable>
+		<input name="Contact Number" type="text" >
+		<lable for="Address"><b>Address</b></lable>
+		<input text="Address" type="text"><br/><br/>
+		<label for="Gender"><b>Gender<b/></label><br/>
+        <input name="Gender" type="radio" value="Male">Male
+        <input name="Gender" type="radio" value="Female" checked="checked">Female<br/><br/>
+		<label for="Password"><b>Password</b></label>
+		<input name="Password" type="Password" value="*********" >
+		<label for="ConfirmPassword"><b>Password</b></label>
+		<input name="ConfirmPassword" type="Password" value="*********"><br/><br/>
+		<input type="button" name="Reset" value="Reset">
+		<input type="submit" name="Sign Up" value="Sign Up">
 
 
+
+
+
+	
+	</form>
 
 <script src="./js/signup.js"></script>
 
--- src/dbConfig.php
+++ src/dbConfig.php
@@ -6,10 +6,10 @@ $password = "";
 $dbName = "book_store";
 
 
-$conn = mysqli_connect($servername, $username, $password, $dbName);
+// $conn = mysqli_connect($servername, $username, $password, $dbName);
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
 
 ?>
