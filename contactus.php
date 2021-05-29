481d60a61ee0bcabc916a5de435abbb781debd1f
SRashani <83983242+SRashani@users.noreply.github.com>
2021-05-29 15:30:51 +0530

    signup
     signup.php and signup.css

--- contactus.php
+++ contactus.php
@@ -7,9 +7,25 @@
 
 
 <!-- Type your Code here -->
-<h1>Contact Us</h1>
 
-
+<form action="contactus.php" method="post">
+		<div class="container">
+	    <h1><center>Contact Us </center></h1>
+    <p><b>Send Us A Massage</b></p>
+		<p>Get in touch with our team and leave the online booking frustrations behind </p>
+		<br/><br/>
+    <a href="https://www.facebook.com/blossom/"> <img id="img1" src="images/fb.jpg"  width ="20px" ></a>&nbsp;&nbsp;&nbsp;
+    <a href="https://www.instagram.com/blossom/"> <img id="img1" src="images/inster.jpg" width ="20px"></a>&nbsp;&nbsp;&nbsp;
+    <a href="https://www.twwiter.com/"><img id="img1" src="images/twitter.jpg" width ="20px" ></a>&nbsp;&nbsp;&nbsp;
+    <br/><br/>
+        
+    <input name="FirstName" type="text" value="First Name">
+    <input text="Last tName" type="text" value="Last Name"><br/><br/>
+    <input text="Email" type="Email"  value="Email">
+    <br/><br/>	
+    <input text="Message" type="text" value="Message">
+    <br/><br/>
+    <input type="button" name="Submit" value="Submit">
 
 <script src="./js/ContactUs.js"></script>
 
