db8042421c8bb1945fce9abd347b02c2071add0e
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-05-30 08:36:27 +0530

    Buyer Seller Dashboard

---
+++ buyerDashboard.php
@@ -0,0 +1,18 @@
+<?php
+  $title = 'Buyer Dashboard'; include("header.php");
+?>
+
+
+<link rel="stylesheet" href="./css/buyerDashboard.css">
+
+
+<!-- Type your Code here -->
+<h1>Buyer Dashboard</h1>
+
+
+
+<script src="./js/buyerDashboard.js"></script>
+
+<?php
+	include "./footer.php";
+?>
---
+++ sellerDashboard.php
@@ -0,0 +1,18 @@
+<?php
+  $title = 'Seller Dashboard'; include("header.php");
+?>
+
+
+<link rel="stylesheet" href="./css/sellerDashboard.css">
+
+
+<!-- Type your Code here -->
+<h1>Seller Dashboard</h1>
+
+
+
+<script src="./js/sellerDashboard.js"></script>
+
+<?php
+	include "./footer.php";
+?>
