4e9bc350827f326210d2f84a16785f08be664470
NTharuka <83983046+NTharuka@users.noreply.github.com>
2021-05-31 10:52:43 +0530

    ne

--- src/dbConfig.php
+++ src/dbConfig.php
@@ -1,23 +1,16 @@
 <?php
 
     // Localhost
-    // $servername = "localhost";
-    // $username = "root";
-    // $password = "";
-    // $dbName = "book_store";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbName = "book_store";
 
-    // මේ file එකේ කිසි දෙයක් වෙනස් කරන්න එපා 
-
-    // Online
-    $servername = "eu-cdbr-west-01.cleardb.com";
-    $username = 'bfacbda18442ae';
-    $password = "a1602e8c";
-    $dbName = "heroku_dbedc9f31577abf";
 
     $conn = mysqli_connect($servername, $username, $password, $dbName);
 
     if (!$conn) {
-        echo '<h1 style="margin-top: 50px; color: red;">Please Turn ON your Wi-Fi</h1>';
+        
         die("Connection failed: " . mysqli_connect_error());
     }
 
